6629475d88aa400e872fe07e2ec7cf5171b4d236
Srejon Khan <srejonkhan79@gmail.com>
2024-02-22 02:47:51 +0600

    feat: add result share post endpoint

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ const router = Router();
 const maintenance = false;
 const prepping = false;
 
+const generateShortId = (id) => {
+	const programCode = id.substring(4, 9);
+	const studentNumberPrefix = id.substring(9, 11);
+	const shortId = btoa(`${programCode}${studentNumberPrefix}`);
+	return shortId.toLowerCase().substring(0, 6);
+};
+
 router
 	.get('/api/list', async (request, env) => {
 		const list = await env.DB.list();
@@ -40,8 +47,48 @@ router
 
 		return json(result);
 	})
-	.get('/share/:sid', ({ sid }) => html(sid))
+	.get('/share/:sid', async (request, env) => {
+		const shareId = request.params.sid;
+		const value = await env.SHARE_DB.get(shareId);
 
+		if (value === null) {
+			return html(notFoundHtml);
+		}
+
+		const result = JSON.parse(value);
+
+		return json(result);
+	})
+	.post('/api/share', async (request, env) => {
+		const body = await request.json();
+
+		if (!body.id || !body.dob) {
+			return error(404, { msg: 'ID or DOB cannot be missing!' });
+		}
+
+		const value = await env.DB.get(body.id);
+
+		if (value === null) {
+			return error(404, { msg: 'Result not found! Recheck your ID and Date of Birth.' });
+		}
+
+		const result = JSON.parse(value);
+		if (result.dob != body.dob) {
+			return error(400, { msg: "DOB doesn't match with provided ID's credential." });
+		}
+
+		const shareId = generateShortId(body.id);
+		const payload = {
+			name: result.name,
+			semester: result.semester,
+			cgpa: result.cgpa,
+			courses: result.courses,
+			shareId: shareId,
+		};
+
+		await env.SHARE_DB.put(shareId, JSON.stringify(payload));
+		return json(payload);
+	})
 	.get('/', () => (maintenance ? html(maintenanceHtml) : prepping ? html(preppingHtml) : html(indexHtml)))
 	// 404 for everything else
 	.all('*', (req) => html(notFoundHtml));
